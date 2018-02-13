5d1043e0a9dabe785c04aa2c375a14e7d70306b5
Fatih Toprakkale <iosbroker2720@gmail.com>
2018-02-13 12:45:15 +0100

    Update raven.js

--- commands/raven.js
+++ commands/raven.js
@@ -1,10 +1,1 @@
-module.exports.run = async (bot, message, args) => {
-    // What will run when the command is called
-   
-  };
-  
-  module.exports.config = {
-    // Config for the command
-    command: "raven"
-  };
-  
+
