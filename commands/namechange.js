56f4e916e59fa2e0e0c8a175cf6342c95ca6b89c
Fatih Toprakkale <iosbroker2720@gmail.com>
2018-02-14 18:54:37 +0100

    Update namechange.js

--- commands/namechange.js
+++ commands/namechange.js
@@ -1,13 +1,15 @@
 module.exports.run = async (bot, message, args) => {  
-  changename();
-  function changename(){
-  var msgList = message.content.split(/\s+/);
-  var name = msgList.toString();
-  console.log(msgList);
-  message.channel.send("Changed your name :)");
-  message.member.setNickname(msgList[1]);
+
+  if(message.channel.id === "404609521579982851"){
+    changename();
+    function changename(){
+      var msgList = message.content.split(/\s+/);
+      var name = msgList.toString();
+      console.log(msgList);
+      message.channel.send("Changed your name :)");
+      message.member.setNickname(msgList[1]);
+    }
   }
-  
   if(message.channel.id != "404609521579982851"){
     message.channel.send("Please use #change-my-name");
   }
