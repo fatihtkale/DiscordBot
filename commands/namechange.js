ec27daec722bd564e8b01f9445d49bb42b9754d9
Fatih Toprakkale <iosbroker2720@gmail.com>
2018-02-14 18:22:41 +0100

    tried someting

--- commands/namechange.js
+++ commands/namechange.js
@@ -1,10 +1,13 @@
 module.exports.run = async (bot, message, args) => {  
   if(message.channel.id === "404609521579982851"){
+    changename();
+  }
+  function changename(){
   var msgList = message.content.split(/\s+/);
   var name = msgList.toString();
   console.log(msgList);
-  message.member.setNickname(msgList[1]);
   message.channel.send("Changed name");
+  message.member.setNickname(msgList[1]);
   }
   if(message.channel.id != "404609521579982851"){
     message.channel.send("Please use #change-my-name");
